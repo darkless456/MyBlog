6e6ee44568afeff74bfd79c0a5c13933c1d49317
Kevin <narcissu456@gmail.com>
2016-01-07 22:51:34 +0800

    echo mysql variables

--- php/unify_navigation.php
+++ php/unify_navigation.php
@@ -3,12 +3,12 @@
  * @Author: darkless
  * @Date:   2015-12-24 15:30:26
  * @Last Modified by:   darkless
- * @Last Modified time: 2015-12-24 15:41:37
+ * @Last Modified time: 2016-01-07 22:51:24
 */
 
 echo '<nav id="main_nav">
         <ul class="nav_ul">
-            <li class="nav_li"><a href="./index.php"><i class="fa fa-home fa-fw"></i>&nbsp;home</a></li>
+            <li class="nav_li"><a href="../index.php"><i class="fa fa-home fa-fw"></i>&nbsp;home</a></li>
             <li class="nav_li"><a href="./py.php"><i class="fa fa-pinterest-square fa-fw"></i>&nbsp;python</a></li>
             <li class="nav_li"><a href="./js.php"><i class="fa fa-book fa-fw"></i>&nbsp;javascript</a></li>
             <li class="nav_li"><a href="./contact.php"><i class="fa fa-pencil fa-fw"></i>&nbsp;contact</a></li>
