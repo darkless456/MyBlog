5da2bda5bbb440b0bd6fe6574444c56134d71e35
Kevin <narcissu456@gmail.com>
2016-01-08 10:08:18 +0800

    update

--- php/connection.php
+++ php/connection.php
@@ -8,16 +8,19 @@
  * @Author: darkless
  * @Date:   2015-12-15 10:48:40
  * @Last Modified by:   darkless
- * @Last Modified time: 2016-01-08 10:03:58
+ * @Last Modified time: 2016-01-08 10:08:07
  */
- // $hostname = SAE_MYSQL_HOST_M. ':'. SAE_MYSQL_PORT;
- // $dbuser = SAE_MYSQL_USER;
- // $dbpass = SAE_MYSQL_PASS;
- // $dbname = SAE_MYSQL_DB; 
- $hostname = 'localhost'. ':'. '3306';
- $dbuser = 'root';
- $dbpass = '';
- $dbname = 'myblog';
+// SAE MySQL
+ $hostname = SAE_MYSQL_HOST_M. ':'. SAE_MYSQL_PORT;
+ $dbuser = SAE_MYSQL_USER;
+ $dbpass = SAE_MYSQL_PASS;
+ $dbname = SAE_MYSQL_DB; 
+
+// Local MySQL
+ // $hostname = 'localhost'. ':'. '3306';
+ // $dbuser = 'root';
+ // $dbpass = '';
+ // $dbname = 'myblog';
  $link = @mysql_connect($hostname, $dbuser, $dbpass);
  if(!$link){
      die("Connect to database failure!". mysql_errno());    
